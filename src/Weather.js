c31ae756ffef25255db5e04475af260cb9769297
Clara Derot <90332021+claradrt@users.noreply.github.com>
2021-11-26 14:17:13 +0100

    Added date and icon on Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate.js";
 
 import Search from "./Search.js";
 
@@ -13,11 +14,15 @@ export default function Weather() {
     console.log(response.data);
     setWeatherData({
       temp: Math.round(response.data.main.temp),
+      country: response.data.sys.country,
+      date: new Date(response.data.dt * 1000),
       city: response.data.name,
       windSpeed: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
+      weather_icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
+    console.log(weatherData.weather_icon);
     setReady(true);
   }
 
@@ -25,7 +30,9 @@ export default function Weather() {
     return (
       <section className="Weather">
         <Search />
-        <h1 id="city-name">{weatherData.city}</h1>
+        <h1 id="city-name">
+          {weatherData.city}, {weatherData.country}
+        </h1>
         <div id="current-location-wrapper">
           <div id="current-location">
             <i className="fas fa-map-marker-alt" id="location-icon"></i>
@@ -43,8 +50,8 @@ export default function Weather() {
             <div className="right-col">
               <div className="current-weather">
                 <img
-                  src="https://openweathermap.org/img/wn/10d@2x.png"
-                  alt="weather-icon"
+                  src={weatherData.weather_icon}
+                  alt={weatherData.description}
                   id="current-weather-icon"
                 />
                 <div
@@ -63,7 +70,9 @@ export default function Weather() {
             </div>
           </div>
         </div>
-        <div className="local-date">Date and time</div>
+        <div className="local-date">
+          <FormattedDate date={weatherData.date} />
+        </div>
         <hr />
       </section>
     );
